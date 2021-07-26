7e8319fdfdafea7d454da9f7e826d0cd171422a8
Frozen0wl <77006798+Frozen0wl@users.noreply.github.com>
2021-07-26 09:50:53 +0200

    Did I Finish my Sudoku

--- Paper/paper.py
+++ Paper/paper.py
@@ -1,6 +1,15 @@
-a_dict = {"a": 1}
-if "a" in a_dict:
-  a_dict["a"] += 1
-else:
-  a_dict["a"] = 1
-print(a_dict)
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+count = (input())
+answer = ""
+for unary in input().split():
+    answer += unary
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(int(answer))
---
+++ codewars/Did I Finish my Sudoku.py
@@ -0,0 +1,41 @@
+def done_or_not(board): #board[i][j]
+    done = True
+    current = []
+    for i in range(len(board)):
+        if [x for x in range(1, 10)] != sorted(board[i]): return "Try again!"
+    
+    for i in range(len(board)):
+        for j in range(len(board)):
+            current.append(board[j][i])
+        if [x for x in range(1, 10)] != sorted(current): return "Try again!"
+        current.clear()
+    
+    for starti in range(0, len(board), 3):
+        for startj in range(0, len(board), 3):
+            for i in range(starti, starti+3):
+                for j in range(startj, startj+3):
+                    current.append(board[i][j])
+            #print(current)
+            if [x for x in range(1, 10)] != sorted(current): return "Try again!"
+            current.clear()
+    return "Finished!"
+
+print(done_or_not([[1, 3, 2, 5, 7, 9, 4, 6, 8]
+                        ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
+                        ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
+                        ,[6, 4, 3, 1, 5, 8, 7, 9, 2]
+                        ,[5, 2, 1, 7, 9, 3, 8, 4, 6]
+                        ,[9, 8, 7, 4, 2, 6, 5, 3, 1]
+                        ,[2, 1, 4, 9, 3, 5, 6, 8, 7]
+                        ,[3, 6, 5, 8, 1, 7, 9, 2, 4]
+                        ,[8, 7, 9, 6, 4, 2, 1, 5, 3]]) == 'Finished!')
+                        
+print(done_or_not([[1, 3, 2, 5, 7, 9, 4, 6, 8]
+                        ,[4, 9, 8, 2, 6, 1, 3, 7, 5]
+                        ,[7, 5, 6, 3, 8, 4, 2, 1, 9]
+                        ,[6, 4, 3, 1, 5, 8, 7, 9, 2]
+                        ,[5, 2, 1, 7, 9, 3, 8, 4, 6]
+                        ,[9, 8, 7, 4, 2, 6, 5, 3, 1]
+                        ,[2, 1, 4, 9, 3, 5, 6, 8, 7]
+                        ,[3, 6, 5, 8, 1, 7, 9, 2, 4]
+                        ,[8, 7, 9, 6, 4, 2, 1, 3, 5]]) == 'Try again!')
