ddc726c79a975191848cf5c9f363cfc24df525ac
Frozen0wl <77006798+Frozen0wl@users.noreply.github.com>
2021-12-31 12:12:03 +0100

    v 2.0

--- small projects/memorize_2.0.py
+++ small projects/memorize_2.0.py
@@ -2,32 +2,48 @@ import pymongo
 import random
 import time
 import bisect
+import datetime
 
 client = pymongo.MongoClient("mongodb+srv://main:main@memorizecluster.guleg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client["Melih"]
 collection = db["Words"]
 
-id = "levels"
 
-words = collection.find_one(id)['words']
+id = input(" | ".join([item["_id"] for item in collection.find()])+": ")
+
+try:
+    words = collection.find_one(id)['words']
+except:
+    print("invalid input")
+    quit()
 
 level_one = 0
 level_two = 3
 level_three = 5
 
-level_one_frequency = 10
-level_two_frequency = level_one_frequency*3
-level_three_frequency = level_one_frequency*7
+day = 3600*24
+
+level_one_frequency = day-3600
+level_two_frequency = day*2.5
+level_three_frequency = day*6
 
 listOfLevels = [[level_one, level_one_frequency], [level_two, level_two_frequency], [level_three, level_three_frequency]]
 intervals = [level_one, level_two, level_three]
 
+
 def update():
     collection.update_one({"_id": id}, {"$set": {'words':words}})
 
+def addMultipleWords():
+    while True:
+        addNewWord(input("word: "), input("meaning: "))
+
 def addNewWord(word, meaning):
-    #TODO check if the word already exists
-    words[word]=[meaning, 0, time.time()]
+    for key in words:
+        if key == word:
+            print("word already exists")
+            return
+    words[word]=[meaning, 0, time.time(), 0] # answer, streak, time, total number of guesses
     update()
 
 def removeWord(word):
@@ -44,8 +60,21 @@ def getInterval(streak):
     i = bisect.bisect_right(intervals, streak)
     return intervals[i-1]
 
-def quiz():
-    
+def printWords():
+    dash = '-' * 48
+    i=0
+
+    for key in words:
+        
+        if i == 0:
+            print(dash)
+            print('{:<10s}{:>18s}{:>10s}{:>10s}'.format("word","meaning","streak","time"))
+            print(dash)
+            i += 1
+
+        print('{:<15s}{:>13s}{:>8d}{:>11.1f}h'.format(key,words[key][0],words[key][1], (time.time()-words[key][2])/3600))
+
+def quiz():    
     for j in range(0, len(listOfLevels)):
         shuffle()
         item = listOfLevels[j]
@@ -58,13 +87,13 @@ def quiz():
                     words[key][1] += 1 # increase the streak
                 else:
                     words[key][1] = 0 # resets the streak
+                    print(f'the correct answer was: {value[0]}')
                 words[key][2] = time.time() #resets the time
+                words[key][3] += 1
                 update()
 
-                    
-                
+       
 
-            
 eval(input("input: "))
 
 
